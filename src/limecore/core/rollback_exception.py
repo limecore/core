4f76546cc90a47136e4d81a7c41815c01f9f9395
Daniel Bradberry <daniel@danielbradberry.com>
2020-04-21 21:42:52 +0100

    Elevate RollbackException

---
+++ src/limecore/core/rollback_exception.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+from .rollback_strategy import RollbackStrategy
+
+
+class RollbackStrategy(Enum):
+    RETRY_NOW = 1
+    RETRY_LATER = 2
+    QUARANTINE = 3
+    NONE = 4
+
+
+class RollbackException(Exception):
+    def __init__(self, *args: object, rollback_strategy: RollbackStrategy):
+        super().__init__(*args)
+
+        self._rollback_strategy = rollback_strategy
+
+    @property
+    def rollback_strategy(self):
+        return self._rollback_strategy
